a4bbc37ab443fb2361c7f27dd4b73c50d9f99f19
Damian Szuster <adverstm@gmail.com>
2022-05-30 00:53:21 +0200

    Created project

---
+++ BazaRoslin/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using BazaRoslin.Services;
+using BazaRoslin.Views;
+using Prism.Ioc;
+
+namespace BazaRoslin {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<Shell>();
+        }
+    }
+}
---
+++ BazaRoslin/Views/Shell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BazaRoslin.Views {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell {
+        public Shell() {
+            InitializeComponent();
+        }
+    }
+}
